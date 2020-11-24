236c6bdf8a20b4b77c78d2ca651b55997ccec979
Dmytro Vasylenko <tredstoun651@uk.net>
2020-11-24 20:31:15 +0200

    добавляет решения дополнительных заданий

--- src/index.js
+++ src/index.js
@@ -2,10 +2,29 @@ import './styles.css';
 import LoadMoreBtn from './js/load-more-btn';
 import PicturesApiServise from './js/apiService'
 import pictureCardTpl from './templates/picture-card.hbs'
+const { error, alert } = require('@pnotify/core')
+import '@pnotify/core/dist/PNotify.css'
+import '@pnotify/core/dist/BrightTheme.css';
+import 'basiclightbox/dist/basicLightbox.min.css'
+import * as basicLightbox from 'basiclightbox'
+
+
 
 const refs = {
     searchForm: document.querySelector('.search-form'),
     gallery: document.querySelector('.gallery'),
+    galleryItem: document.querySelector('.photo-card')
+}
+refs.gallery.addEventListener('click', onImageClick)
+
+function onImageClick(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+    const originalPicture = event.target.dataset.source
+    const instance = basicLightbox.create(`<img src="${originalPicture}" width="800" height="600">`)
+
+  instance.show()
 }
 
 const loadMoreBtn = new LoadMoreBtn({selector: '[data-action="load-more"]', hidden: true})
@@ -21,23 +40,30 @@ function onSearch(e) {
     picturesApiService.query = e.currentTarget.elements.query.value.trim()
 
     if (picturesApiService.query === '') {
-        return
+        loadMoreBtn.hide() 
+        return alert({
+              text: "Введите что-нибуть!",
+              type: 'info'
+        })
+       
     }
 
     picturesApiService.resetPage()
-    loadMoreBtn.show()
     fetchPictures()
+    loadMoreBtn.show()
 }
 
 function onLoadMore() {
-
+     
+    const startTime = performance.now()
     fetchPictures()
-    scroll()
-    console.log(refs.gallery.clientHeight) 
+    const endTime = performance.now()
+    const time = Math.floor(endTime - startTime) * 1000
+    scroll(time)
+  
+}  
 
-}
-
-function appendPicturesMarkup(pictures) {
+ function appendPicturesMarkup(pictures) {
     refs.gallery.insertAdjacentHTML('beforeend', pictureCardTpl(pictures))
 }
 
@@ -45,18 +71,44 @@ function clearPictureGallery() {
      refs.gallery.innerHTML = ''
 }
 
-function fetchPictures() {
-    loadMoreBtn.disable()
-    picturesApiService.fetchPictures().then(pictures => {
-        appendPicturesMarkup(pictures);
-        loadMoreBtn.enable()
-    });
+  async function fetchPictures() {
+     loadMoreBtn.disable();
+
+      try {
+         const pictures = await picturesApiService.fetchPictures();
+         appendPicturesMarkup(pictures);
+         loadMoreBtn.enable();
+         
+      } catch (e) {
+          errors(e)
+          loadMoreBtn.hide() 
+     }
+      
+    //   
+    //   picturesApiService.fetchPictures().then(pictures => {
+    //      appendPicturesMarkup(pictures);
+    // loadMoreBtn.enable()
+    //  })
+    
 }
 
-function scroll() {
+function scroll(time) {
     let i = refs.gallery.clientHeight;
     setTimeout(() => {
-window.scrollTo({ top: i, behavior: 'smooth' });
-    }, 500)
-    
+        window.scrollTo({ top: i, behavior: 'smooth' });
+    }, time)
+}
+
+function errors(e) {
+    if (e === 'ничего не найдено') {
+      return  error({
+            text: 'К сожалению по этому запросу ничего не найдено',
+            type: 'info'
+        })
+    }
+
+    return error({
+             text: "Ошибка! Не удалось загрузить изображения.",
+             type: 'info' 
+          })
 }
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -7,15 +7,24 @@ export default class PicturesApiServise {
         this.page = 1;
     }
 
-    fetchPictures() {
+   async fetchPictures() {
         const url = `${BASE_URL}/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`
 
-        return fetch(url)
-        .then(response => response.json())
-            .then(data => {
-                this.page += 1;
-                return data.hits;
-        })
+       const fetches = await fetch(url)
+       const json = await fetches.json()  
+       if (json.total === 0) {
+         throw 'Ничего не найдено'
+       }
+       this.page += 1;
+       
+       return json.hits;
+       
+        // return fetch(url)
+        // .then(response => response.json())
+        //     .then(data => {
+        //         this.page += 1;
+        //         return data.hits;
+        // })
     }
 
     resetPage() {
